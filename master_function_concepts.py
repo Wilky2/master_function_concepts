0d43be5f0de4a8a31920af5655cce238b4dc1569
Wilky2 <wilkyclervil@yahoo.com>
2022-12-12 13:51:50 -0500

    amelioration de la presentation

--- master_function_concepts.py
+++ master_function_concepts.py
@@ -1,27 +1,22 @@
 import string,random
 
-def generate_code_without_char_repeat(code_length,select_list):
-    code = ""
-    select_list_copy = select_list.copy()
-    for i in range(code_length):
-        char = random.choice(select_list_copy)
-        select_list_copy.remove(char)
-        code+=str(char)
-    return code
-
 #egzeis 3
 def generate_code_with_alphabetical_char(code_length):
     select_list = list(string.ascii_lowercase)
-    return generate_code_without_char_repeat(code_length,select_list)
-    
-print(generate_code_with_alphabetical_char(6))
+    return "".join(random.sample(select_list,code_length))
+
+print("egzesis 3")
+print("Code de longueur 6")
+print(f"Code : {generate_code_with_alphabetical_char(6)}\n")
 
 #egzesis 4
 def generate_code_with_alphanumeric_char(code_length):
-    select_list = list(string.ascii_lowercase) + list(range(10))
-    return generate_code_without_char_repeat(code_length,select_list)
+    select_list = list(string.ascii_lowercase) + [f"{i}" for i in range(10)]
+    return "".join(random.sample(select_list,code_length))
 
-print(generate_code_with_alphanumeric_char(6))
+print("egzesis 4")
+print("Code de longueur 7")
+print(f"Code : {generate_code_with_alphanumeric_char(7)}\n")
 
 #egzesis 5
 def replace_all_special_char(string):
@@ -37,7 +32,10 @@ def generate_slug(string):
     string_without_special_char = replace_all_special_char(string)
     return string_without_special_char.replace(" ","-")
 
-print(generate_slug("Clervil Wilky"))    
+slug =  generate_slug("!Cl&rvil Wilky")
+print("egzesis 5")
+print("slug de la chaine : !Cl&rvil Wilky")
+print(f"SLUG : {slug}\n")  
  
 
 #egzesis 6
@@ -45,29 +43,31 @@ def separate_char_with_comma(word):
     list_of_char = list(word)
     return ",".join(list_of_char)
 
-print(separate_char_with_comma("Wilky"))
+result = separate_char_with_comma("Wilky")
+print("egzesis 6")
+print("Mot choisi : Wilky")
+print(f"resultat : {result}\n")
 
 #egzesis 7
 def crypt_word_with_alphabet_index(word):
-    alphabet = list(string.ascii_lowercase)
-    word_lower = word.lower()
-    word_crypt=""
-    for char in word_lower:
-        word_crypt+=str(alphabet.index(char)) + "-"
-    return word_crypt[0:len(word_crypt)-1]
+    word_crypt= "-".join([str(ord(char)-97) for char in word.lower()])
+    return word_crypt
 
-print(crypt_word_with_alphabet_index("ALO"))
+crypt = crypt_word_with_alphabet_index("ALO")
+print("egzesis 7")
+print("Mot a decrypter : ALO")
+print(f"cryptage : {crypt}\n")
 
 #egzesis 8
 def decrypt_word_with_alphabet_index(word_crypt):
-    alphabet = list(string.ascii_lowercase)
     list_of_index = word_crypt.split("-")
-    word=""
-    for index in list_of_index:
-        word+=alphabet[int(index)]
+    word="".join([chr(int(index)+97) for index in list_of_index])
     return word
 
-print(decrypt_word_with_alphabet_index("0-11-14"))
+decrypt = decrypt_word_with_alphabet_index("0-11-14")
+print("egzesis 8")
+print("Code : 0-11-14")
+print(f"Mot : {decrypt}\n")
 
 #egzesis 9
 def swap(parameter1,parameter2):
@@ -76,7 +76,10 @@ def swap(parameter1,parameter2):
     parameter2=temp
     return (parameter1,parameter2)
 
-print(swap("Wilky","Clervil"))
+swp = swap("Wilky","Clervil")
+print("egzesis 9")
+print("valeur : Wilky,Clervil")
+print(f"resultat : {swp}\n")
 
 #egzesis 10
 def list_of_name_component(name):
@@ -93,4 +96,7 @@ def initial_name(name):
         initial+=component[0]
     return initial
 
-print(initial_name("Jean-Baptiste Jean"))
+init_name = initial_name("Jean-Baptiste Jean")
+print("egzesis 10")
+print("Nom : Jean-Baptiste Jean")
+print(f"initial name : {init_name}")
